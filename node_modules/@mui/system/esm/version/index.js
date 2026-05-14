6bc7463b5979e942dbff0d0993a294d5c716cef6
Arseniy Abramidze <zowel.aep@mail.ru>
2026-05-14 10:00:15 +0300

    rebuild

--- node_modules/@mui/system/esm/version/index.js
+++
@@ -1,6 +0,0 @@
-export const version = "7.3.11";
-export const major = Number("7");
-export const minor = Number("3");
-export const patch = Number("11");
-export const prerelease = undefined;
-export default version;
--- node_modules/@mui/utils/useForcedRerendering/useForcedRerendering.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Copied from @base-ui/utils
- *
- * Returns a function that forces a rerender.
- */
-export default function useForcedRerendering(): () => void;
